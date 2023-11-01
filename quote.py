df2c8fb189f50a2987364e09db900b05e5b74f10
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-01 11:21:18 +0530

    Create quote.py
    simple program using variables, strings and print() funciton.

---
+++ quote.py
@@ -0,0 +1,7 @@
+#Initializing the variables
+name = "Albert Einstein"
+filler = "once said"
+quote = '"A person who never made a mistake never tried anything new"'
+
+#printing a message using theose predefined variables
+print(f"{name} {filler} {quote}")
