2d341078a443500828ad01f1f86354e024b0b969
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-03 10:13:07 +0530

    Create lists.py
    #Store the names of a few of your friends in a list called names.
    #Print each person’s name by accessing each element in the list, one at a time.

---
+++ lists.py
@@ -0,0 +1,14 @@
+#Store the names of a few of your friends in a list called names.
+#Print each person’s name by accessing each element in the list, one at a time.
+
+#creating a list cantaining items as friend names
+friends = ['Narayana', 'Govinda', 'Hari', 'Krishna','Gopal', 'Rama', 'Radha']
+
+#printing each friend's name by accessing each element
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[5])
+print(friends[6])
