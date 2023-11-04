659968783fad4d5621f1aebd7ee088474e329e72
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-04 08:38:46 +0530

    Create lists_vehicles.py
    # Making a list of my favorite mode of transportation, such as a car, bike, bus etc that stores several examples. Using that list 
    i'll print a series of statements about these items, such as “I would like to own a Honda motorcycle.

---
+++ lists_vehicles.py
@@ -0,0 +1,11 @@
+# Making a list of my favorite mode of transportation, such as a car, bike, bus etc that stores several examples. Using that list 
+i'll print a series of statements about these items, such as “I would like to own a Honda motorcycle.
+
+#Creating a List containing my favorite modes of transportation
+favorite_vehicles = ["Tesla Cybertruck","TATA Bus", "Mercedes Benz", "Hundai", "Honda"]
+
+#Printing a statement for each item in the list
+print(f"I would like to own a {favorite_vehicles[0]} within next 5 years")
+print(f" Sometime travel using {favorite_vehicles[1]}")
+print(f"A neighbor of mine owns a {favorite_vehicles[3]}")
+print(f"{favorite_vehicles[3]} and {favorite_vehicles[4]} are common car companies in India")
