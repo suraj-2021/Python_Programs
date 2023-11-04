ab31a0f504eced4247a67d58d0b40a9d9f8edb39
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-04 09:07:50 +0530

    Create list_modify.py
    #I will create a list and modify it.

---
+++ list_modify.py
@@ -0,0 +1,10 @@
+#I will create a list and modify it.
+
+#creating a list and assigning it to a variable called "rivers"
+rivers = ['Ganga','Yamuna','Godavari','Saraswati','Narmada','Sindhu','Kavari']
+
+#modifying the list; for Kavari I'll add Gandaki, for Sindhu I'll add Bagmati.
+rivers[6]= 'Gandaki'
+rivers[5] = 'Bagmati'
+
+print(rivers)
