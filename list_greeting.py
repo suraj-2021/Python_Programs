ceb459e9c1354157be86a301ba5c2e3fbf7ad3ef
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-04 08:26:05 +0530

    Create list_greeting.py
    #Creating a program which will greet individual person in the list

---
+++ list_greeting.py
@@ -0,0 +1,10 @@
+#Creating a program which will greet individual person in the list
+
+#variable "friends" is assigned to a list containing friend names
+friends = ['Keshav', 'Govind','Narayana','Hari']
+
+#printing a greeting for each friend
+print(f"Hello {friends[0]}, Good Morning!")
+print(f"Namaste {friends[1]}, Subh Prabhat")
+print(f"Namasker {friends[2]}, Ram Ram")
+print(f"Radhe Radhe {friends[3]}")
