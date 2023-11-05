e70b1c2abcd7a939c97cf5265e41e7d48b128835
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-05 08:24:51 +0530

    Create list_removing_item.py
    #backtobasics practicing the basics again!

---
+++ list_removing_item.py
@@ -0,0 +1,9 @@
+#Creating a list and remove an item using the remove() method.
+
+names = ['Shree', 'Krishna','Govinda','Hare','Murare','Natha','Narayana','Vahsudeva']
+
+#removing a name from the list
+names.remove('Hare')
+
+#printing the list
+print(names)
