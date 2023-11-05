4423f3b764c67a91dd3b76354a6611d671039895
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-05 08:53:09 +0530

    Create list_guests.py
    #Creating a list of professionals I want to invite to dinner.

---
+++ list_guests.py
@@ -0,0 +1,9 @@
+#Creating a list professionals i want to invite to dinner.
+
+guests = ['Kishan','Teacher','Soldier','helper','Doctor','Driver or transporter','Lawyer','DJ','Event-Manager',]
+
+print(f"I want to invite {guests[0]} because, he is a Farmer and food is a fundamental necessity for life!")
+print(f"I want to invite {guests[1]} because, he is an Educator and Education is needed for happy and productive life!")
+print(f"I want to invite {guests[2]} because, he is a  and Devender Safety and Security is needed for clear thinking and creativity!")
+print(f"I want to invite {guests[3]} because, he helps people with vision, in society like we need visionaries we need helping hands to bring vision to life!")
+print(f"I want to invite {guests[4]} because, he teaches you healthy habits and avoid unhealthy activities!")
