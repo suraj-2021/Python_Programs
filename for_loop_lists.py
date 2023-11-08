2ce0ae83159889c009d9c6fff8f8e68a38c5b87c
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-08 08:32:45 +0530

    for_loop_lists.py

---
+++ for_loop_lists.py
@@ -0,0 +1,7 @@
+#creating a list of mountain and printing each item in the list using the for loop.
+
+mountains = ['Sagarmatha','Dhaulagiri','Kanchanjunga','Machapuchhre','Manaslou','Lhotse']
+
+#printing each item in the list using for loop and print() function.
+for mountain in mountains:
+    print(mountain)
