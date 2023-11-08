01b802035d30d6bf626330f28fb9bc91a5618a4d
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-08 09:18:52 +0530

    looping_pizzas.py

---
+++ looping_pizzas.py
@@ -0,0 +1,8 @@
+#Making a list of pizzas, use the for loop to assign each item in the list and assign to a variable, and print a statement using each item.
+
+pizzas = ['Mushroom','Onion','Pepperoni','Spicy Pineapple']
+
+#Looping through the list using the for loop.
+for pizza in pizzas:
+  print(f"I love the taste of {pizza} pizza!")
+  
