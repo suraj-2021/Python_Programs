66b5086c853b280866e10b4b981876c7bd56b4ab
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-09 09:13:21 +0530

    # Three different animals that have a common characteristic. Storing the names of these animals in a list, and then use a for loop to print out the name of each animal.
    # Three different animals that have a common characteristic. Storing the names of these animals in a list, and then use a for loop to print out the name of each animal.

---
+++ list_animals.py
@@ -0,0 +1,12 @@
+# Three different animals that have a common characteristic. Storing the names of these animals in a list, and then use a for loop to print out the name of each animal.
+
+#creating the list
+animals = ['Cat','Lion','Tiger','Cheetah','Panther','Jaguar','Puma',]
+
+#looping through the list and printing each item on the list.
+for animal in animals:
+    print(animal)
+
+#printing a statement for each animal
+for animal in animals:
+    print(f"{animal} belongs to felidae family!")
