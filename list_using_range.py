65df0435ba652b509ac65a191224c8b3fee3c687
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-10 21:09:49 +0530

    list_using_range.py

---
+++ list_using_range.py
@@ -0,0 +1,9 @@
+#creating a list using range function
+
+print(list(range(1,19)))
+
+#creating a list using for loop and range function
+numbers = []
+for i in range(1,19):
+    numbers.append(i)
+    print(numbers)
