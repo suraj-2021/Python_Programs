14bf111cb3074f32ea7ceca8ceda5bf436b0403c
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-11 09:11:46 +0530

    area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -4,7 +4,7 @@
 pi = 22/7
 radius = input("Please provide me with radius of your circle: ")
 
+
 #Calculating the area of the circle
-if radius.is_number():
-   area = pi*radius**2
-   print(f"The area of your circle with radius {radius} is :{area}")
+area = pi*radius**2
+print(f"The area of your circle with radius {radius} is :{area}")
