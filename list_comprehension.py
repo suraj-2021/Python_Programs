3f5f9e40f41ed4a4ee0060172409a25615184b82
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-12 08:43:31 +0530

    list_comprehension.py
    #Using a list comprehension to generate a list of the first 10 cubes

---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+#Using a list comprehension to generate a list of the first 10 cubes
+
+#creating a list using list-comprehension.
+cubes = [c**3 for c in range(1,11)]
+
+#printing out the list
+print(cubes)
