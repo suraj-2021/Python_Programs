404150ec85b17b6d6493c6bb5c94e250a0aaf954
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-12 08:04:18 +0530

    multiples_of_3.py
    #Making a list of the multiples of 3 from 3 to 30. Using a for loop to print the numbers in your list.

---
+++ multiples_of_3.py
@@ -0,0 +1,10 @@
+#Making a list of the multiples of 3 from 3 to 30. Using a for loop to print the numbers in your list.
+
+#creating a list and appending the multiples of 2 to the list
+list = []
+for i in range(3,30,3):
+   
+    list.append(i)
+
+#printing the list
+print(list)
