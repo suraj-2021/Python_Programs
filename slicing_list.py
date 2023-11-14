2073ee7ca5b8445040f705bfb7d3c35cab483593
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-14 08:24:55 +0530

    slicing_list.py
    #Creating a list and do slicing operations on it.

---
+++ slicing_list.py
@@ -0,0 +1,16 @@
+#Creating a list and do slicing operations on it.
+
+#creating a list of Names
+names = ['Shree','Krishna','Govinda','Hare','Murare','Hey','Natha','Narayana','Vasushadeve','Gopal','Radha','Ramana']
+
+#creating a slice of first three names from the list and printing the slice.
+first_three = names[0:3]
+print(first_three)
+
+#creating a slice with three names from the middle of the list and printing the slice.
+middle_names = names[4:9]
+print(middle_names)
+
+#creating a slice with last three name from the list and printing the slice.
+last_names = names[9:]
+print(last_names)
