6352de774bc291e22402eed5253a2a74f4e827e0
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-16 08:52:51 +0530

    conditional_tests.py
    #Conditional Tests: Writing a series of conditional tests. Printing a statement describing each test and my prediction for the results of each test.

---
+++ conditional_tests.py
@@ -0,0 +1,12 @@
+#Conditional Tests: Writing a series of conditional tests. Printing a statement describing each test and my prediction for the results of each test.
+
+car = 'Toyota'
+print("Is car =='Toyota'? I pridict it's true.")
+print(car=='Toyota')
+
+bike = 'Honda'
+print("Is bike =='Honda',? I predict it's true.")
+print(bike=='Honda')
+truck = 'Tesla Cybertruck'
+print("Is truck =='TATA'? I predict False")
+print(truck=='TATA')
