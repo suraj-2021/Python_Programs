02f213e5352ce0ce2f9c1632abc529392713327e
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-17 08:09:57 +0530

    Create conditional.py

---
+++ conditional.py
@@ -0,0 +1,16 @@
+#Tests for equality and inequality with strings
+#Tests using the lower() method
+#Numerical tests involving equality and inequality, greater than and 
+#less than, greater than or equal to, and less than or equal to
+#Tests using the and keyword and the or keyword
+#Test whether an item is in a list
+#Test whether an item is not in a list.
+
+#test with strings
+name_one = 'Radha'
+name_two = 'Krishna'
+#returns false
+name_one == name_two
+
+
+
