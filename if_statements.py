51d7c263691658eb4b795ca1510bcf3e7c28c43c
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-19 07:23:36 +0530

    if_statements.py
    #Making a list of fruits and checking weather a fruit in list, if yes print a statement.

---
+++ if_statements.py
@@ -0,0 +1,17 @@
+#Making a list of fruits and checking weather a fruit in list, if yes print a statement.
+
+favorite_fruits = ['banana','strawberry','mango','kafal','kiwi','guava']
+
+if'orange' in favorite_fruits:
+   print("You really like Oranges!")
+else:
+  print("No oranges!")
+  
+if'mango' in favorite_fruits:
+   print("You really love Mangoes!")
+   
+if'kiwi' in favorite_fruits:
+   print("You really like Kiwis")
+   
+if'guava' in favorite_fruits:
+   print("You really like guava")
