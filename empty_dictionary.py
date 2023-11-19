c6b184cdbfbca391d21e252a6f8aaa9ae636f4d8
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-19 15:08:54 +0530

    empty_dictionary.py
    #starting with empty dictionary and later add keys and values to it.

---
+++ empty_dictionary.py
@@ -0,0 +1,11 @@
+#starting with empty dictionary and later add keys and values to it.
+
+#empty dictionary
+person = {}
+
+#adding key:value pairs to the empty dictionary we already created.
+person['Name'] = 'Shree'
+person['Age'] = 24
+
+#printing the dictionary
+print(person)
