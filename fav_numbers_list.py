88e65d088603db27a552f41bb0f6a32f9c1e703f
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-20 08:33:01 +0530

    fav_numbers_list.py
    #Crating a list with name of friends being key and and their favorite numbers being the value, later printing the name and favorite numbers.

---
+++ fav_numbers_list.py
@@ -0,0 +1,9 @@
+#Crating a list with name of friends being key and and their favorite numbers being the value, later printing the name and favorite numbers.
+
+#creating a list
+friendly_list = {'shree':1008,'Krishna':8,'Govinda':100,'Hare':200,'Murare':48}
+
+#printing each name and their favorite numebrs.
+for name,number in friendly_list.items():
+    print(f"Friend's name is {name} and their fevorite number is {number}")
+  
