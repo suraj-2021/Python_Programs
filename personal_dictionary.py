27d59468ab85ff1d6d59fdb722c4d0c2718c66d6
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-20 08:13:13 +0530

    personal_dictionary.py
    #Create a dictionary including personal information of a person and printing each information.

---
+++ personal_dictionary.py
@@ -0,0 +1,9 @@
+#Create a dictionary including personal information of a person and printing each information.
+
+personal_dictionary = {'Name':'Saraswati','Age':24,'Location':'Osaka','Dream':'Scientist'}
+
+#printing each information stored in the dictionary
+print(personal_dictionary['Name'])
+print(personal_dictionary['Age'])
+print(personal_dictionary['Location'])
+print(personal_dictionary['Dream'])
