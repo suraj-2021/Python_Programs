aab7b49b8170780afeb23a953b70f3aaf258b19e
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-21 09:13:09 +0530

    dictionary_pairs.py
    #Creating a dictionary and lopping throuh it to print the keys and values.

---
+++ dictionary_pairs.py
@@ -0,0 +1,8 @@
+#Creating a dictionary and lopping throuh it to print the keys and values.
+
+dictionary = {'Name':'Shree','Age': 18,'Address':'Seashore road 12th block','Education':'Graduate'}
+
+#looping through our newly created dictionary to print out the content of our dictionary
+for k,v in dictionary.items():
+    print(f"{k} {v}")
+  
