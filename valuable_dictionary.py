0e46915ceb0eb1f9a701eb23e33cf1a187758630
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-25 07:50:49 +0530

    valuable_dictionary.py
    #Add keys and values to a dictionary using Square Brackets Notation

---
+++ valuable_dictionary.py
@@ -0,0 +1,10 @@
+#Add keys and values to a dictionary using Square Brackets Notation
+
+# Create an empty dictionary
+my_di = {}
+
+# Add a new key-value pair
+my_di['name'] = 'John'
+
+# Print the updated dictionary
+print(my_di)
