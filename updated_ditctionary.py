bbf84a0c85e4ea6a3e80279ec164ba7ce57d6b50
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-25 07:57:54 +0530

    Using update() method
    #Using update() method to add multiple key-value pairs to a dictionary at once.

---
+++ updated_ditctionary.py
@@ -0,0 +1,10 @@
+#Using update() method to add multiple key-value pairs to a dictionary at once.
+
+# Create an empty dictionary
+my_di = {}
+
+# Add multiple key-value pairs
+my_di.update({'name': 'John', 'age': 30, 'city': 'Seattle'})
+
+# Print the updated dictionary  
+print(my_di)
