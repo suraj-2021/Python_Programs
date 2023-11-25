814449509eeb7f8bee502bc173bf6ec7de03f828
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-11-25 08:01:22 +0530

    Key-Value pairs using the dict() constructor.
    #Create a new dictionary with key-value pairs using the dict() constructor.

---
+++ dicticted_dictionary.py
@@ -0,0 +1,7 @@
+#Create a new dictionary with key-value pairs using the dict() constructor.
+
+# Create a dictionary using the dict() constructor
+my_dict = dict(name='John', age=30, city='Seattle')
+
+# Print the dictionary
+print(my_dict)
