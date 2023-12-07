29c50478c45acfeca957e3ac38d8130ba99b7bf2
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-12-07 08:14:31 +0530

    city_striker.py
    #A function has default parameter called country, and a non-default parameter called city.

---
+++ city_striker.py
@@ -0,0 +1,10 @@
+#A function has default parameter called country,and a non default parameter called City.
+
+def country_city(city,country="Nepal"):
+    print(f"{city} is in {country}")
+    
+
+country_city('Kathmandu')
+country_city('Bhaktapur')
+country_city('Narayangad')
+country_city('Viratnagar')
