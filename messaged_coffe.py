c8a3be32db9ec2c9eda561082abcb7fe8d1b7e36
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-12-07 08:02:33 +0530

    messaged_coffe.py
    #Personalized coffee cups have a default Message. 
    #The following function,takes size as argument and Message is a default one.

---
+++ messaged_coffe.py
@@ -0,0 +1,12 @@
+#Personalized coffee cups have a default Message. 
+#The following function,takes size as argument and Message is a default one.
+
+def make_large_cup(size,message='I Love Python'):
+    print(f"\nOur Cup's size is: {size}")
+    print(f"\nMessage on our Cup is: {message}")
+    
+make_large_cup(2)
+make_large_cup(3,'Joe rogan wakes up early in the Morning')
+make_large_cup(4,'Lex Fridman is brillient Scientist')
+make_large_cup(5,'Andrew Huberman is Academically Successful')
+make_large_cup(3)
