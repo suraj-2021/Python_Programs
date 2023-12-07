f7bd103c82145987053a9836edef81a8b8a5f563
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-12-07 07:52:13 +0530

    messaged_cups.py
    #Let's make our own personalized coffee cups. 
    #The following function,takes size and message to be printed on our cup as parameters/argument while calling the function.

---
+++ messaged_cups.py
@@ -0,0 +1,12 @@
+#Let's make our own personalized coffee cups. 
+#The following function,takes size and message to be printed on our cup as parameters/argument while calling the function.
+
+def make_cup(size,message):
+    print(f"\nOur Cup's size is: {size}")
+    print(f"\nMessage on our Cup is: {message}")
+    
+make_cup(2,'Working Hard')
+make_cup(3,'Joe rogan wakes up early in the Morning')
+make_cup(4,'Lex Fridman is brillient Scientist')
+make_cup(5,'Andrew Huberman is Academically Successful')
+make_cup(3,'David Goggins is Hard Worker')
