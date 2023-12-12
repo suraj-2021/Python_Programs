7d429d42df2a926d876eed2e4a7f8a0a128fc8db
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2023-12-12 07:43:52 +0530

    text_messages.py
    #A function that prints a text-messages stored in a list.

---
+++ text_messages.py
@@ -0,0 +1,9 @@
+#A function that prints a text-messages stored in a list.
+
+def show_messages(messages):
+    for message in messages:
+        print(message)
+        
+text_messages = ['Hello Good Morning','How are you','Lets get some insights','lets have some consturctive fun']
+
+show_messages(text_messages)
