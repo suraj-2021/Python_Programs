55f241558e982d3ec857340787299d22de29d6fe
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-01-09 08:35:27 +0530

    file_reader.py
    #We are accessing a text file and read through that file. Later we print the contents.

---
+++ file_reader.py
@@ -0,0 +1,9 @@
+#We are accessing atext file and read through that file. Later we print the contents 
+
+with open('python_learnings.txt') as main_file:
+    x_file = main_file.read()
+    print(x_file)
+    print(f"\n\n{x_file}")
+    print(f"\n\n{x_file}")
+    print(f"\n\n{x_file}")
+    print(f"\n\n{x_file}")
