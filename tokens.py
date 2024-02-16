054ca8bb82cecad192b4e179ece8dd0d0e3a7134
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-02-16 21:17:09 +0530

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,9 @@
+# tokens.py
+
+# This is a sample tokens file. Replace 'YourTokenHere' with your actual tokens.
+
+TOKENS = {
+    'auth_token': 'YourTokenHere',
+    'api_token': 'YourTokenHere',
+    'access_token': 'YourTokenHere'
+}
