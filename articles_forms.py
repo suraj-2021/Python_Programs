efb55749c85bd093fd1d58efa9cb0f74d15f88a6
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-02-23 21:23:57 +0530

    Create articles_forms.py

---
+++ articles_forms.py
@@ -0,0 +1,8 @@
+# myapp/forms.py
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ["pub_date", "headline", "content", "reporter"]
