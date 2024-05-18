2faba922f851ad1840c26e211aea12b884266ff9
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-05-18 20:59:20 +0530

    Create super_simple_views.py

---
+++ super_simple_views.py
@@ -0,0 +1,14 @@
+# views.py
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+  
+    return HttpResponse("Hello, world!")
+
+def about(request):
+  
+    return HttpResponse("About page")
+
+
