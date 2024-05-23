82a135fb9487882e1c4d7409d2dfffca94749fa3
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-05-23 21:12:38 +0530

    a_typical_urls.py
    urls.py file

---
+++ a_typical_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from your_app_name import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+]
