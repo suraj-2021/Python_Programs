75d3b72e21bd3f1587be307295e4687c825acc6b
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-06-12 09:18:37 +0530

    django_intermediatory _model.py
    The intermediatory model is the connection that holds the extra information (like the date of purchase) about the Many-to-Many relationship.
    So, while you’re not using the Purchase model directly, you’re still relying on its existence and structure to retrieve the data you need.

---
+++ django_intermediatory _model.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Mango(models.Model):
+    color = models.CharField(max_length=128)
+    mango_from = models.CharField(max_length=128)  # Corrected typo here
+
+class Person(models.Model):
+    name = models.CharField(max_length=80)
+    bought = models.ManyToManyField('Mango', through='Purchase')
+
+class Purchase(models.Model):
+    mango = models.ForeignKey('Mango', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    date = models.DateField()
