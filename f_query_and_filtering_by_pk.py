06ea320aa2e39adb6d4e9e08fce587acd4296d69
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-06-18 09:17:24 +0530

    f_query_and_filtering_by_pk.py
    This is a models.py file where we compare two different fields of the same model using F query,

---
+++ f_query_and_filtering_by_pk.py
@@ -0,0 +1,14 @@
+from django.db.models import F
+from django.db import models
+
+
+class BigMountains(models.Model):
+      name = models.CharField(max_legth=150)
+      height = models.IntegerField()
+      peak_temprature = models.IntegerField()
+      climbers = models.IntegerField()
+
+
+mountains = BigMountains.objects.filter(peak_temprature__lt=F("climbers"))
+
+BigMountains.objects.filter(pk__gt=16)
