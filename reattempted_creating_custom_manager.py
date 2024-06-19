6ba32d53958f63c0d3532f1ec3548d06c2b827b4
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-06-19 08:29:18 +0530

    reattempted_creating_custom_manager.py
    We have created a custom manager by overriding the get_queryset method of Django’s built-in models.Manager. 
    
    Our ActiveUserStatus manager filters the queryset to return only the users where the is_active attribute is True. 
    
    This attribute corresponds directly to the is_active field in our User model, establishing a relationship between the two. Thus, when we call User.active_users.all(), it utilizes this custom manager to retrieve only active users

---
+++ reattempted_creating_custom_manager.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class ActiveUserStatus(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    objects = models.Manager() 
+    active_users = ActiveUserStatus()  
