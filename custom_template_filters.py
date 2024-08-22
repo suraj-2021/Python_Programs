14e289a7a78ddd94008f3eb7cfb55d2330a71c9c
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-08-22 20:56:55 +0530

    Create custom_template_filters.py
    Created custom template filter for templates that converts passed context to lowercase.

---
+++ custom_template_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def lower(value):
+    """Converts a string into all lowercase."""
+    return value.lower() if isinstance(value, str) else value
