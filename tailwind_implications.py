f56e3420d663da69ae171bc15bb81895409261ad
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-09-10 21:15:08 +0530

    tailwind_implications.py
    Experimenting with tailwind CSS to see its value for Django templates.

---
+++ tailwind_implications.py
@@ -0,0 +1,14 @@
+<div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+  <img class="w-full" src="####" alt="Card image">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Title</div>
+    <p class="text-gray-700 text-base">
+    Hello How are you today!
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tag1</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tag2</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tag3</span>
+  </div>
+</div>
