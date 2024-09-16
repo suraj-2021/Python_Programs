5c7c1ccc7d4179dfc43484e9de832ebac9a92253
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-09-16 20:40:29 +0530

    Create websocket_javascript.py
    1. It establishes a WebSocket connection with the server using the room name as part of the URL.
    2. When a message is received from the server, it parses the JSON data and appends the message to the chat log.
    3. It focuses on the chat message input field and listens for the Enter key press, which triggers the message submission.
    4. When the user clicks the submit button or presses Enter, it retrieves the message from the input field, sends it to the server as a JSON object, and clears the input field.

---
+++ websocket_javascript.py
@@ -0,0 +1,27 @@
+<script>
+    const roomName = "{{ room_name }}";
+    const chatSocket = new WebSocket(
+        'ws://' + window.location.host + '/ws/chat/' + roomName + '/'
+    );
+
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        document.querySelector('#chat-log').innerHTML += '<div>' + data.message + '</div>';
+    };
+
+    document.querySelector('#chat-message-input').focus();
+    document.querySelector('#chat-message-input').onkeyup = function(e) {
+        if (e.keyCode === 13) {  // Enter key
+            document.querySelector('#chat-message-submit').click();
+        }
+    };
+
+    document.querySelector('#chat-message-submit').onclick = function(e) {
+        const messageInputDom = document.querySelector('#chat-message-input');
+        const message = messageInputDom.value;
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+        messageInputDom.value = '';
+    };
+</script>
