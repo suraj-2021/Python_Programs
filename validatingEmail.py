13487c2f2747addbb0edbe608f5d5c38901a71b2
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2025-11-06 09:05:44 +0530

    email validation and filtering using REGEX
    Implement email validation and filtering functionality

---
+++ validatingEmail.py
@@ -0,0 +1,17 @@
+import re
+def fun(s):
+   if re.match(r"^([a-zA-Z0-9\-\_])@([a-zA-Z0-9])\.([a-zA-Z]{3})$",s):
+       return s
+    
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
